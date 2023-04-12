985de2d4488ae4f751ec7b2d62a9d714829d0715
YUNUS EMRE ÇELİK <yunusemree60@gmail.com>
2023-04-12 10:46:09 +0300

    BookStory

--- Layer.Repository/Seeds/CategorySeed.cs
+++ Layer.Repository/Seeds/CategorySeed.cs
@@ -14,13 +14,15 @@ namespace Layer.Repository.Seeds
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.HasData(
-                new Category { CategoryID= 1, CategoryName = "Macera" },
+                new Category { CategoryID=  1, CategoryName = "Macera" },
                 new Category { CategoryID = 2, CategoryName = "Tarih" },
                 new Category { CategoryID = 3, CategoryName = "Dünya Klasikleri" },
                 new Category { CategoryID = 4, CategoryName = "Hikaye" },
                 new Category { CategoryID = 5, CategoryName = "BilimKurgu" },
                 new Category { CategoryID = 6, CategoryName = "Türk Edebiyatı" },
-                new Category { CategoryID = 7, CategoryName = "Roman" }
+                new Category { CategoryID = 7, CategoryName = "Roman" },
+                new Category { CategoryID = 8, CategoryName = "Polisiye"},
+                new Category { CategoryID = 9, CategoryName = "Drama"}
                 );
         }
     }
